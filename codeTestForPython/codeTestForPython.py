6c7210e6ae82c867374f41375d0d2bc2616d86e5
jy0605 <31639823+jy0605@users.noreply.github.com>
2022-04-05 21:02:45 +0900

    초기 커밋.

---
+++ codeTestForPython/codeTestForPython.py
@@ -0,0 +1,77 @@
+# 식목일의 코드트리 1번
+'''
+print('           *********')
+print('      *****         ****')
+print('    **                  **')
+print('   *                      *')
+print(' **                        **')
+print('*                            *')
+print('*                            *')
+print('**                          **')
+print(' **  *    ****  ****    *  **')
+print('   ** ***** ****** ***** **')
+print('             ****')
+print('             ****')
+print('             ****')
+print('             ****')
+print('            ******')
+print('           ********')
+'''
+# 식목일의 코드트리 2번
+'''
+n = input()
+fi_input = input().split(' ')
+fi = list()
+
+for i in range(int(n)):
+  fi.append(int(fi_input[i]))
+
+fi_cp = fi.copy()
+
+maxFi = max(fi)
+minFi = min(fi_cp)
+fi.pop(fi.index(maxFi))
+fi_cp.pop(fi_cp.index(minFi))
+nextMaxFi = max(fi)
+nextMinFi = min(fi_cp)
+maxMultiplyValue = maxFi * nextMaxFi;
+minMultiplyValue = minFi * nextMinFi;
+
+resMultiplyValue = maxMultiplyValue if maxMultiplyValue > minMultiplyValue else minMultiplyValue;
+print(resMultiplyValue)
+'''
+
+
+# 식목일의 코드트리 3번
+n, m = map(int, input().split(' '))
+arrField = [list() for i in range(n)]
+
+print(arrField)
+
+for i in range(n):
+    thisLineInfo = map(int, input().split(' '))
+    arrField[i] = list(thisLineInfo)
+
+firstL, firstR = map(int, input().split(' '))
+secL, secR = map(int, input().split(' '))
+
+for i in range(firstL-1, firstR, 1):
+  try:
+    indexOfCriminal = arrField[i].index(1)
+    arrField[i][indexOfCriminal] = 0
+  except:
+    pass
+
+numberOfRestCriminals = 0
+
+for i in range(secL-1, secR, 1):
+  try:
+    indexOfCriminal = arrField[i].index(1)
+    arrField[i][indexOfCriminal] = 0
+  except:
+    pass
+
+for i in range(n):
+  numberOfRestCriminals += arrField[i].count(1)
+
+print(numberOfRestCriminals)
